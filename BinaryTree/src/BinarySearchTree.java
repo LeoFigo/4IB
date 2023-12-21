a94fee7816e951603f5667759868383058835f17
LeoFigo <leonardo.dalessio@itiszuccante.edu.it>
2023-12-21 08:38:42 +0100

    quasi finito

--- BinaryTree/src/BinarySearchTree.java
+++ BinaryTree/src/BinarySearchTree.java
@@ -19,7 +19,7 @@ public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<Intege
         return r;
     }
     public void delete(Integer val) {
-        if (!cerca(val, root)) {
+        if (!search(val, root)) {
             System.out.println("valore " + val + " non presente");
         } else {
             root = deleteRecursive(val, root);
@@ -53,5 +53,77 @@ public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<Intege
             r = r.getLeft();
         }
         return min;
+    }
+    public void inserisciIterattivo(Integer valore) {
+        if (root == null) {
+            root = new Node<Integer>(valore);
+        } else {
+            Node<Integer> current = root;
+            while (current != null) {
+                if (valore < current.getValue()) {
+                    if (current.getLeft() == null) {
+                        current.setLeft(new Node<Integer>(valore));
+                        return;
+                    } else {
+                        current = current.getLeft();
+                    }
+                } else if (valore > current.getValue()) {
+                    if (current.getRight() == null) {
+                        current.setRight(new Node<Integer>(valore));
+                        return;
+                    } else {
+                        current = current.getRight();
+                    }
+                } else {
+                    System.out.println("valore già presente");
+                    return;
+                }
+            }
+        }
+    }
+
+    public void deleteInterattivo(Integer valore) {
+        if (!search(valore, root)) {
+            System.out.println("valore " + valore + " non presente");
+        } else {
+            Node<Integer> current = root;
+            Node<Integer> parent = null;
+            while (current != null) {
+                if (valore < current.getValue()) {
+                    parent = current;
+                    current = current.getLeft();
+                } else if (valore > current.getValue()) {
+                    parent = current;
+                    current = current.getRight();
+                } else {
+                    if (current.getLeft() == null) {
+                        if (parent == null) {
+                            root = current.getRight();
+                        } else {
+                            if (current.getValue() < parent.getValue()) {
+                                parent.setLeft(current.getRight());
+                            } else {
+                                parent.setRight(current.getRight());
+                            }
+                        }
+                    } else if (current.getRight() == null) {
+                        if (parent == null) {
+                            root = current.getLeft();
+                        } else {
+                            if (current.getValue() < parent.getValue()) {
+                                parent.setLeft(current.getLeft());
+                            } else {
+                                parent.setRight(current.getLeft());
+                            }
+                        }
+                    } else {
+                        Node<Integer> min = findMin(null,current.getRight());
+                        current.setValue(min.getValue());
+                        current.setRight(deleteRecursive(min.getValue(), current.getRight()));
+                    }
+                    return;
+                }
+            }
+        }
     }
 }
--- BinaryTree/src/BinaryTree.java
+++ BinaryTree/src/BinaryTree.java
@@ -6,7 +6,24 @@ public class BinaryTree<E> {
     public BinaryTree(Node<E> root) {
         this.root = root;
     }
-
+    public Boolean search(Integer val, Node<E> node) {
+        if(node == null) {
+            return false;
+        } else if (node.getValue().equals(val)) {
+            return true;
+        } else {
+            return search(val, node.getLeft()) || search(val, node.getRight());
+        }
+    }
+    public Integer findMin(Integer i, Node<Integer> node) {
+        if(node == null) {
+            return null;
+        } else if (i.compareTo(node.getValue()) < 0) {
+            return i;
+        } else {
+            return findMin(i, node.getLeft());
+        }
+    }
 
 
     public static class Node<E> {
