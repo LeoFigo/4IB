c262476147e7dd1d057fbf208e542ea11ffce749
LeoFigo <leonardo.dalessio@itiszuccante.edu.it>
2024-02-26 13:04:45 +0100

    da vedere

--- GestioneConForm/script.js
+++ GestioneConForm/script.js
@@ -6,29 +6,37 @@ function createObj() {
     let nome = document.getElementById('fname').value;
     let descrizione = document.getElementById('fdesc').value;
     let prezzo = document.getElementById('fprezzo').value;
-    let sconto = document.querySelector('input[name="p"]:checked').value;
-    let inMagazzino = document.getElementById('magazz').checked;
-
-    // Create a new object to store product details
+    //let sconto = document.querySelector('input[name="p"]:checked').value;
+    //let inMagazzino = document.getElementById('magazz').checked;
+    
+    // Crea un nuovo oggetto
     let product = {
         category: category,
         codice: codice,
         nome: nome,
         descrizione: descrizione,
         prezzo: prezzo,
-        sconto: sconto,
-        inMagazzino: inMagazzino
+        //sconto: sconto,
+        //inMagazzino: inMagazzino
     };
+
     prodotti.push(product);
-    //showProducts();
-    // You can do further processing with the 'product' object, like sending it to a server, storing it locally, or displaying it on the page.
-    console.log(product); // For demonstration, logging the product object to the console
+    console.log("ciao")
+    showProducts();
+    
+    console.log(product);
+    
 }
 
 function showProducts() {
     let output = "";
+    let list = document.getElementById("product-list");
+    list.innerHTML = "";
     for (let i = 0; i < prodotti.length; i++) {
-        output += "<li>" + prodotti[i].nome + " " + prodotti[i].codice + " " + prodotti[i].descrizione + " " + prodotti[i].prezzo + "€" + "</li>";
+        let li = document.createElement('li');
+        li.innerHTML = prodotti[i].nome + " " + prodotti[i].codice + " " + prodotti[i].descrizione + " " + prodotti[i].prezzo + "€";
+        list.appendChild(li);
+
     }
-    document.getElementById("product-list").innerHTML = output;
+
 }
