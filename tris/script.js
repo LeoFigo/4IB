c33a4427af11218d393c407822b204df2f60890e
LeoDesk <leonardo.dalessio@itiszuccante.edu.it>
2024-03-15 00:28:10 +0100

    v1

--- tris/script.js
+++ tris/script.js
@@ -1,24 +1,39 @@
 let matrix = [[null, null, null], [null, null, null], [null, null, null]];
-let cond = false;
-
+let cond = true;
+let mosse = 0;
 // x = 1 = true
+checkFirst();
 function add(r, c) {
     if (matrix[r][c] == null) {
         if (cond) {
-            matrix[r][c] = 2;
+            matrix[r][c] = 4;
             let s = r + "" + c;
             document.getElementById(s).style.backgroundColor = 'green';
+            document.getElementById("player").style.backgroundColor = 'red';
         } else {
             matrix[r][c] = 1;
             let s = r + "" + c;
             document.getElementById(s).style.backgroundColor = 'red';
+            document.getElementById("player").style.backgroundColor = 'green';
         }
+        mosse++;
+        document.getElementById("mosse").innerText = `Mosse: ${mosse}`;
         if (checkWin()) {
             close();
         } else {
             cond = !cond;
         }
     }
+
+}
+
+function checkFirst() {
+    if (cond) {
+        document.getElementById("player").style.backgroundColor = 'green';
+    } else {
+        document.getElementById("player").style.backgroundColor = 'red';
+    }
+    document.getElementById("mosse").innerText = `Mosse: ${mosse}`;
 
 }
 
@@ -45,27 +60,39 @@ function checkWin() {
         temp = 0;
         for (let j = 0; j < matrix[i].length; j++) {
             temp = temp + matrix[i][j];
-            if (temp == 3 || temp == 6) {
+            if (temp == 3 || temp == 12) {
                 return true;
             }
         }
     }
-    
-    for (let i = 0; i < matrix[i].length; i++) {
+
+    for (let i = 0; i < matrix.length; i++) {
         temp = 0;
-        for (let j = 0; j < matrix.length; j++) {
+        for (let j = 0; j < matrix[i].length; j++) {
             temp = temp + matrix[j][i];
-            if (temp == 3 || temp == 6) {
+            if (temp == 3 || temp == 12) {
                 return true;
             }
         }
     }
-    /*
-    if ((matrix[0, 0] + matrix[1, 1] + matrix[2, 2]) == 3 || (matrix[2, 0] + matrix[1, 1] + matrix[0, 2]) == 3) {
+
+    if ((matrix[0][0] + matrix[1][1] + matrix[2][2]) == 3 || (matrix[2][0] + matrix[1][1] + matrix[0][2]) == 12) {
         return true;
-    } else if ((matrix[2, 0] + matrix[1, 1] + matrix[0, 2]) == 3 || (matrix[2, 0] + matrix[1, 1] + matrix[0, 2]) == 6) {
+    } else if ((matrix[2][0] + matrix[1][1] + matrix[0][2]) == 3 || (matrix[2][0] + matrix[1][1] + matrix[0][2]) == 12) {
         return true;
     }
+
     return false;
-    */  
+
+}
+function reset() {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            matrix[i][j] = null;
+            document.getElementById(i + "" + j).style.backgroundColor = 'white';
+        }
+    }
+    cond = true;
+    mosse = 0;
+    checkFirst();
 }
