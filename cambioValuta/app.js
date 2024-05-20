3c80c9bc3f69947a485cacfb447d8dcae66a18bc
LeoFigo <leonardo.dalessio@itiszuccante.edu.it>
2024-05-20 13:48:14 +0200

    flags

--- cambioValuta/app.js
+++ cambioValuta/app.js
@@ -1,6 +1,10 @@
-let valuta1 = document.getElementById("valuta1").value;
-let valuta2 = document.getElementById("valuta2").value;
-let value = [];
+let valuta1 = document.getElementById("valuta1");
+let valuta2 = document.getElementById("valuta2");
+let importo1 = document.getElementById("importo1");
+let importo2 = document.getElementById("importo2");
+let bandiera1 = document.getElementById("bandiera1");
+let bandiera2 = document.getElementById("bandiera2");
+let valute = [];
 fetch("http://localhost:3002/valute")
     .then(response => response.json())
     .then(data => {
@@ -9,15 +13,29 @@ fetch("http://localhost:3002/valute")
         console.log(valute);
         for (let i = 0; i < valute.length; i++) {
             console.log(valute[i].name);
-            let option = document.createElement("option");
-            option.text = valute[i].name;
-            option.id = valute[i].id; 
-            valuta1.appendChild(o);
-            valuta2.appendChild(o);
+        
+            let option1 = document.createElement("option");
+            option1.text = valute[i].name;
+            option1.value = valute[i].id; 
+            valuta1.appendChild(option1);
+            bandiera1.innerHTML = "<img src='Bandiere/" + valute[valuta1.value].fileName + "' alt='bandiera' style='max-width: 100px;'>";
+        
+            let option2 = document.createElement("option");
+            option2.text = valute[i].name;
+            option2.value = valute[i].id; 
+            valuta2.appendChild(option2);
+            bandiera2.innerHTML = "<img src='Bandiere/" + valute[valuta2.value].fileName + "' alt='bandiera' style='max-width: 100px;'>";
         }
     });
 
 function converti() {
+    /*console.log(importo1.value); 
+    console.log(valute[valuta2.value].tasso);*/
+    bandiera1.innerHTML = "<img src='Bandiere/" + valute[valuta1.value].fileName + "' alt='bandiera' style='max-width: 100px;'>";
+    bandiera2.innerHTML = "<img src='Bandiere/" + valute[valuta2.value].fileName + "' alt='bandiera' style='max-width: 100px;'>";
+    let val1 = eval(importo1.value / valute[valuta1.value].tasso);
 
-
+    let res = eval(val1 * valute[valuta2.value].tasso);
+    
+    importo2.value = res;
 }
